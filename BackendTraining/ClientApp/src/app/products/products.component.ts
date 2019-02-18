93a72ff42c5b94a35803446d3825c1858eb5cc5b
Domício Medeiros <domicioam@gmail.com>
2019-02-18 20:18:03 -0300

    Creating table for products and fake products data.

--- BackendTraining/ClientApp/src/app/products/products.component.ts
+++ BackendTraining/ClientApp/src/app/products/products.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  public products: Product[];
 
   constructor() { }
 
   ngOnInit() {
+    //get from backend after implementing security
+    this.products = [
+      { description: 'Bottle of water', quantity: 10, price: 1.5 },
+      { description: 'French fries', quantity: 15, price: 2.5 },
+      { description: 'Snack', quantity: 7, price: 5 },
+    ]
   }
 
+}
+
+interface Product {
+  description: string;
+  quantity: number;
+  price: number;
 }
